e26c77fd46f7d3f99ccae947d4fb5022d3c546fd
Matthew Malkin <matt@zercat.net>
2016-05-29 23:29:41 +0100

    change nuget restore path

--- NuGet/restore.ps1
+++ NuGet/restore.ps1
@@ -1,4 +1,4 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
 Write-Host "Root: $root"
 
-& $root\NuGet\NuGet.exe restore $root\ZConfigTests
+& $root\NuGet\NuGet.exe restore $root\ZConfig.sln
